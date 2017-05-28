175670ed404716a78f679e96c0a607ae191ddd38
safonovklim <safonovklim@gmail.com>
2017-05-28 14:58:58 +0300

    Added dailyGame (service, model, messages). Added sequelize migration for that.

---
+++ scripts/migrations/20170519193131-Added_dailyGame_model.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const tableName = 'daily_game_log';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface
+      .createTable(tableName, {
+        selectedAt: {
+          type: Sequelize.DataTypes.DATE,
+          allowNull: false,
+        }
+      }, {
+        timestamps: false,
+      })
+      .then(() => Promise.all([
+        queryInterface.addColumn(tableName, 'ChatId', {
+          type: Sequelize.DataTypes.STRING,
+        }),
+        queryInterface.addColumn(tableName, 'UserId', {
+          type: Sequelize.DataTypes.INTEGER,
+        })
+      ]))
+      .then(() => Promise.all([
+        queryInterface.sequelize.query('ALTER TABLE ONLY "' + tableName + '" ADD CONSTRAINT "'+  tableName +'_UserId_fkey" FOREIGN KEY ("UserId") REFERENCES users(id) ON UPDATE CASCADE ON DELETE SET NULL;'),
+        queryInterface.sequelize.query('ALTER TABLE ONLY "' + tableName + '" ADD CONSTRAINT "'+  tableName +'_ChatId_fkey" FOREIGN KEY ("ChatId") REFERENCES chats(id) ON UPDATE CASCADE ON DELETE SET NULL;'),
+      ]));
+  },
+
+  down: function (queryInterface) {
+    return queryInterface.dropTable(tableName);
+  }
+};
